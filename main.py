2e6acc5636687b47db3bed5da4c22b94ab6151da
annapotr <potryasaeva.anna@mail.ru>
2021-09-15 19:21:23 +0300

    add line

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import math
 import matplotlib.pyplot as plt
 
 from fastapi import FastAPI, HTTPException
@@ -22,42 +21,80 @@ app.add_middleware(
 app.mount("/client", StaticFiles(directory="client"), name="client")
 
 
+class Point(BaseModel):
+    name: str
+    x: float
+    y: float
+
+    def get(self):
+        return self.x, self.y
+
+
+class Line(BaseModel):
+    name: str
+    x1: float
+    y1: float
+    x2: float
+    y2: float
+
+    def get(self):
+        return self.x1, self.x2, self.y1, self.y2
+
+
+d = dict()
+flags = {
+    'point': 'bo',
+    'line': 'g-'
+}
+
+
+def check_name(name: str):
+    if name == "":
+        raise HTTPException(status_code=400, detail="Invalid name")
+
+
+def add_item(item, item_type):
+    check_name(item.name)
+    if item.name not in d:
+        d[item.name] = dict()
+    if item_type not in d[item.name]:
+        d[item.name][item_type] = list()
+    d[item.name][item_type].append(item.get())
+
+
+def draw_figure(name):
+    plt.cla()
+    for key, items in d[name].items():
+        for i in items:
+            plt.plot(i[:len(i) // 2], i[len(i) // 2:], flags[key])
+    file_path = f'{name}.png'
+    plt.savefig(file_path)
+    return file_path
+
+
 @app.get("/")
 async def redirect():
     return RedirectResponse(url='/client/index.html')
 
 
-@app.get("/items/")
-def read_item(item_id: int = 0):
+@app.get("/id")
+def get_id(item_id: int = 0):
     return {"id": item_id}
 
 
-class ItemIn(BaseModel):
-    name: str
-    x: float
-    y: float
+@app.post("/add/point")
+async def add_point(item: Point):
+    add_item(item, 'point')
 
 
-d = dict()
+@app.post("/add/line")
+async def add_line(item: Line):
+    add_item(item, 'line')
 
 
-@app.post("/items/")
-async def create_item(item: ItemIn):
-    if item.x == 0 and item.y == 0:
-        raise HTTPException(status_code=400, detail="Invalid request")
-    if item.name in d:
-        d[item.name].append((item.x, item.y))
-    else:
-        d[item.name] = [(item.x, item.y)]
-    return math.sqrt(item.x ** 2 + item.y ** 2)
+@app.get("/draw")
+def draw(name: str = ""):
+    check_name(name)
+    return FileResponse(draw_figure(name))
 
-
-@app.get("/draw/{name}")
-def draw(name: str):
-    plt.cla()
-    for (x, y) in d[name]:
-        plt.plot(x, y, 'bo')
-    file_path = f'{name}.png'
-    plt.savefig(file_path)
-    return FileResponse(file_path)
 
